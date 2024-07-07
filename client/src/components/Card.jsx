442f650c6b4691e71576addd09ab61e8035abffa
abhishek nyamati <abhisheknyamati@gmail.com>
2024-07-08 04:00:00 +0530

    thoda css

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,13 @@
 
 function Card(){
     return(
-        <div className="p-5">
+        <div className="p-5 flex flex-wrap">
+            <div className="w-52 border-2 h-64 p-5 border-black font-mono m-5">
+                <div className="title text-xl font-bold">Title</div>
+                <div className="desc text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab mollitia aliquam soluta blanditiis cumque natus optio explicabo maxime facere aperiam.</div>
+                <div className="author text-sm font-semibold">@abhishek_nyamati</div>
+                <div className="time text-sm bottom-0 right-0">11:11 am</div>
+            </div>
             <div className="w-52 border-2 h-64 p-5 border-black font-mono">
                 <div className="title text-xl font-bold">Title</div>
                 <div className="desc text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab mollitia aliquam soluta blanditiis cumque natus optio explicabo maxime facere aperiam.</div>
